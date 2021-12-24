d865c8e65ca48e3540cc14acaf02d9ff10303c59
Manuk-Han <aksdnr507@gmail.com>
2021-12-24 17:40:11 +0900

    태그 기능

--- src/main/java/com/studyolle/settings/SettingsController.java
+++ src/main/java/com/studyolle/settings/SettingsController.java
@@ -27,13 +27,15 @@ import javax.validation.Valid;
 public class SettingsController {
 
     static final String SETTINGS_PROFILE_VIEW_NAME ="settings/profile";
-    static final String SETTINGS_PROFILE_URL="/settings/profile";
+    static final String SETTINGS_PROFILE_URL="/"+SETTINGS_PROFILE_VIEW_NAME;
     static final String SETTINGS_PASSWORD_VIEW_NAME ="settings/password";
-    static final String SETTINGS_PASSWORD_URL="/settings/password";
+    static final String SETTINGS_PASSWORD_URL="/"+SETTINGS_PASSWORD_VIEW_NAME;
     static final String SETTINGS_NOTIFICATIONS_VIEW_NAME ="settings/notifications";
-    static final String SETTINGS_NOTIFICATIONS_URL ="/settings/notifications";
+    static final String SETTINGS_NOTIFICATIONS_URL ="/"+SETTINGS_NOTIFICATIONS_VIEW_NAME;
     static final String SETTINGS_ACCOUNT_VIEW_NAME ="settings/account";
-    static final String SETTINGS_ACCOUNT_URL ="/settings/account";
+    static final String SETTINGS_ACCOUNT_URL ="/"+SETTINGS_ACCOUNT_VIEW_NAME;
+    static final String SETTINGS_TAGS_VIEW_NAME ="settings/tags";
+    static final String SETTINGS_TAGS_URL ="/"+SETTINGS_TAGS_VIEW_NAME;
 
     private final AccountService accountService;
     private final ModelMapper modelMapper;
@@ -131,5 +133,11 @@ public class SettingsController {
         accountService.updateNickname(account, nicknameForm.getNickname());
         attributes.addFlashAttribute("message","닉네임을 수정했습니다.");
         return "redirect:"+SETTINGS_ACCOUNT_URL;
+    }
+
+    @GetMapping(SETTINGS_TAGS_URL)
+    public String updateTage(@CurrentUser Account account, Model model){
+        model.addAttribute(account);
+        return SETTINGS_TAGS_URL;
     }
 }
